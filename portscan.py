3a6bbb4e4ecef70d149b385f8201899ba997181f
No_Name.exe <153848626+NoNameZusy@users.noreply.github.com>
2024-04-21 22:49:34 +0400

    Add files via upload

---
+++ portscan.py
@@ -0,0 +1,31 @@
+import socket
+
+def port_scan(target_host):
+    print("[*] Starting port scan on", target_host)
+    for port in range(1, 65536):
+        try:
+            # Socket oluştur
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1)
+
+            # Portu tara
+            result = s.connect_ex((target_host, port))
+            if result == 0:
+                print("[+] Port {} is open".format(port))
+            s.close()
+        except KeyboardInterrupt:
+            print("\n[*] Exiting due to user interrupt")
+            exit()
+        except socket.gaierror:
+            print("[!] Hostname could not be resolved")
+            exit()
+        except socket.error:
+            print("[!] Couldn't connect to server")
+            exit()
+
+def main():
+    target_host = input("Enter target IP address: ")
+    port_scan(target_host)
+
+if __name__ == "__main__":
+    main()
